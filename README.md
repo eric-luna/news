1d7014c65267841115579b015e6feaa11f3b3387
Eric <ericluna1992@gmail.com>
2017-01-16 17:03:43 -0600

    the basic structure of the app has been established.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# news
+# News
+
+[![N|Solid](http://www.siteshotter.com/website-thumbnail/newsapi.org)](https://www.themoviedb.org/?language=en)
+
+Cinema Seeker/TV Seeker is a tool for movie and tv lovers to look up information on their favortie movies or tv shows. 
+
+Features:
+  - Top Headlines
+  - Popular Categories
+  - Trusted News Sources
+
+### Tech
+
+News was bult using:
+
+* Angular.js
+* Node.js
+* AJAX
+* HTML
+* CSS
+* Responsive Design
+
---
+++ app/general/general.js
@@ -0,0 +1,54 @@
+'use strict';
+
+angular.module('myApp.general', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/general', {
+    templateUrl: 'general/general.html',
+    controller: 'GeneralCtrl',
+  });
+}])
+
+.controller('GeneralCtrl', ['$http', function($http) {
+	var self=this;
+	self.source = 'google-news';
+	self.currentSource = 'Google News';
+
+	self.sources = [
+        {source: 'google-news',name:'Google News'},
+        {source: 'associated-press',name:'Associated Press'},
+        {source: 'bbc-news',name:'BBC News'}, 
+        {source: 'cnn',name:'CNN'}, 
+        {source: 'independent', name:'Independent' },
+        {source: 'newsweek', name:'Newsweek'}, 
+        {source: 'new-york-magazine',name:'New York Magazine' },
+        {source: 'reuters',name:'Reuters'},
+        {source: 'the-guardian-uk', name:'The Guardian'},
+        {source: 'reddit-r-all',name:'Reddit r/all'},
+        {source: 'the-huffington-post',name:'The Huffington Post'},
+        {source: 'the-new-york-times',name:'The New York Times'},
+        {source: 'the-washington-post',name:'The Washington Post'},
+        {source: 'time',name:'Time'},
+        {source: 'usa-today',name:'USA Today'}
+      ];
+
+    self.initial = self.sources[0].source;
+
+    self.changeSource = function(source){
+    	$http.get('https://newsapi.org/v1/articles?source='+source+'&apiKey=22480be10ea14a548027c1172f4beee6').then(function(response) {
+    		self.headlines = response.data.articles;
+    		// self.currentSource = name;
+    		angular.forEach(self.sources, function(value, key) {
+    			if(source === value.source){
+    				self.currentSource = value.name;
+    			}
+			});
+		});
+    }
+
+	$http.get('https://newsapi.org/v1/articles?source='+self.sources[0].source+'&apiKey=22480be10ea14a548027c1172f4beee6').then(function(response) {
+    	self.headlines = response.data.articles;
+	});
+
+	
+}]);
